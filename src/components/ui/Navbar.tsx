e882be3efb0d1eef13ec2027a752724c84f84f5a
“Emman” <emmanuelruiz258@gmail.com>
2025-06-18 13:12:58 +0800

    initial commit

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import { Button } from "./button";
+import { HomeIcon, Sprout } from "lucide-react";
+import ModeToggle from "../ModeToggle";
+
+function Navbar() {
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link
+              href={"/"}
+              className="text-xl font-bold text text-primary font-mono tracking-wider"
+            >
+              🌱 Plantventory
+            </Link>
+          </div>
+
+          {/* Nav Components */}
+          <div className="hidden md:flex items-center space-x-4">
+            <ModeToggle />
+
+            <Button
+              variant={"ghost"}
+              className="flex items-center gap-2"
+              asChild
+            >
+              <Link href={"/plants"}>
+                <Sprout className="w-4 h-4" />
+                <span className="hidden lg:inline">Plants</span>
+              </Link>
+            </Button>
+
+            <Button
+              variant={"ghost"}
+              className="flex items-center gap-2"
+              asChild
+            >
+              <Link href={"/"}>
+                <HomeIcon className="w-4 h-4" />
+                <span className="hidden lg:inline">Home</span>
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
