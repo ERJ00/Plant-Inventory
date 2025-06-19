54a48a895c418e0379e022964a11a1bbae156076
“Emman” <emmanuelruiz258@gmail.com>
2025-06-19 12:30:58 +0800

    added authentication

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import { Button } from "./button";
-import { HomeIcon, Sprout } from "lucide-react";
+import { HomeIcon, LogIn, LogOut, Sprout } from "lucide-react";
 import ModeToggle from "../ModeToggle";
+import { stackServerApp } from "@/stack";
+import { getUserDetails } from "@/actions/users.actions";
 
-function Navbar() {
+async function Navbar() {
+  const user = await stackServerApp.getUser();
+  const app = stackServerApp.urls;
+  const userProfile = await getUserDetails(user?.id);
+
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
@@ -43,6 +49,36 @@ function Navbar() {
                 <span className="hidden lg:inline">Home</span>
               </Link>
             </Button>
+
+            {user ? (
+              <>
+                {/* Sign out button */}
+                <Button
+                  variant={"ghost"}
+                  className="flex items-center gap-2"
+                  asChild
+                >
+                  <Link href={app.signOut}>
+                    <LogOut className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign Out</span>
+                  </Link>
+                </Button>
+              </>
+            ) : (
+              <>
+                {/* Sign in button */}
+                <Button
+                  variant={"ghost"}
+                  className="flex items-center gap-2"
+                  asChild
+                >
+                  <Link href={app.signIn}>
+                    <LogIn className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign In</span>
+                  </Link>
+                </Button>
+              </>
+            )}
           </div>
         </div>
       </div>
